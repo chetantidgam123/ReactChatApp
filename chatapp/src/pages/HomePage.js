6ab5c6bcbccf5e4e8c213f26c34ace763e92dd6b
chetan <chetan@shipyaari.com>
2023-01-11 18:57:55 +0530

    frontend

--- chatapp/src/pages/HomePage.js
+++ chatapp/src/pages/HomePage.js
@@ -1,10 +1,36 @@
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW={'xl'} centerContent>
+      <Box display={'flex'}
+        justifyContent={'center'}
+        p={3}
+        bg={'white'}
+        w='100%'
+        borderRadius='lg'
+        borderWidth="1px"
+        m={"40px 0 15px 0"}>
+        <Text fontSize='4xl' fontFamily="Work sans" color="black">Talk-A-Tive</Text>
+      </Box>
+      <Box bg={'white'} w='100%' p={4} borderRadius='lg' borderWidth="1px" >
+        <Tabs variant='soft-rounded'>
+          <TabList mb={'1em'}>
+            <Tab width={'50%'} >Login</Tab>
+            <Tab width={'50%'} >Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
